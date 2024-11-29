c051d86e9c271da17a4f0654bc81732d12580b6b
fa0311 <yukinashi2742756@yahoo.co.jp>
2024-11-29 10:12:25 +0000

    update

---
+++ ichigo2025/utils/export.sh
@@ -0,0 +1,4 @@
+python3 onnx_export.py  \
+    --net=mb2-ssd-lite  \
+    --labels=models/output/labels.txt  \
+    --input=models/output/mb2-ssd-lite-Epoch-49-Loss-0.6414913535118103.pth
--- ichigo2025/utils/train_ssd.sh
+++ ichigo2025/utils/train_ssd.sh
@@ -6,9 +6,20 @@ python3 train_ssd.py \
     --model-dir=models/output \
     --num-workers=8 \
     --batch-size=16 \
-    --num-epochs=100 \
-    --lr=0.001 \
-    --freeze-base-net \
-    --extra-layers-lr=0.01 \
+    --num-epochs=50 \
     --validation-epochs=1 \
-    --balance-data
+    --lr=0.01 \
+    --base-net-lr=0.002 \
+    --extra-layers-lr=0.01 \
+    --weight-decay=0.000001
+
+# python3 train_ssd.py \
+#     --pretrained-ssd=models/mb2-ssd-lite-mp-0_686.pth \
+#     --net=mb2-ssd-lite \
+#     --dataset-type=voc \
+#     --data=data/voc/berries_project_JA-PascalVOC-export \
+#     --model-dir=models/output_old \
+#     --num-workers=8 \
+#     --batch-size=16 \
+#     --num-epochs=50 \
+#     --validation-epochs=1
